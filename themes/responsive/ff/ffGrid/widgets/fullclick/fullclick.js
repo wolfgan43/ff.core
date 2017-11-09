331628c32a33d9db912ba6b3f9836d61b9d7c62b
Mirko Di Pasquale <supporto@xodusweb.com>
2017-11-09 10:31:11 +0000

    fullclick.js edited online with Bitbucket

--- themes/responsive/ff/ffGrid/widgets/fullclick/fullclick.js
+++ themes/responsive/ff/ffGrid/widgets/fullclick/fullclick.js
@@ -15,6 +15,8 @@
 							eval(jQuery(this).closest("TR.clickable").data("url"));
 						else if(jQuery(this).closest("TR.clickable").hasClass("ajax"))
 							ff.ffGrid.dialogOpen(jQuery(this).closest("DIV.ffGrid").data("record"), jQuery(this).closest("TR.clickable").data("url"), undefined, jQuery(this).closest("TR.clickable"));
+						else if(jQuery(this).closest("TR.clickable").data("target"))
+						    window.open(jQuery(this).closest("TR.clickable").data("url"));
 						else
 							window.location.href = jQuery(this).closest("TR.clickable").data("url");
 					}).css("cursor", "pointer");
@@ -28,6 +30,8 @@
 							eval(jQuery(this).data("url"));
 						else if(jQuery(this).hasClass("ajax"))
 							ff.ffGrid.dialogOpen(jQuery(this).closest("DIV.ffGrid").data("record"), jQuery(this).data("url"), undefined, jQuery(this));
+						else if(jQuery(this).data("target"))
+						    window.open(jQuery(this).data("url"));
 						else
 							window.location.href = jQuery(this).data("url");
 					}).css("cursor", "pointer");					
