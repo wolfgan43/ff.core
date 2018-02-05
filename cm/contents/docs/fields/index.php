c729eee2fe1306a0ed443dd89e568df07b8a542e
Davide Vento <davidevento1998@gmail.com>
2018-02-05 16:23:39 +0100

    Selection Field added

--- cm/contents/docs/fields/index.php
+++ cm/contents/docs/fields/index.php
@@ -167,6 +167,86 @@ $oRecord->addContent(null, true, $group_field);
 $oRecord->groups[$group_field] = array(
 	"title" => ucfirst($group_field)
 );
+$oRecord->addContent('<hr /><h2 class="' . cm_getClassByFrameworkCss(array(12), "col") . '">Campo di selezione (Selection)</h2>', $group_field);
+
+$oField = ffField::factory($cm->oPage);
+$oField->id="utenti";
+
+$oField->extended_type = "Selection"; //Questa istruzione dirà che questo è un campo speciale, che nel nostro caso si chiamerà "Selection"
+/*
+ * Nella source_SQL va inserita una SELECT che selezionerà solamente due campi
+ * Il primo sarà quello salvato nel database
+ * Il secondo sarà quello visualizzato dall'utente
+ */
+$oField->source_SQL = "  SELECT ID, username FROM cm_mod_security_users
+                          WHERE 1 
+                          [AND] [WHERE] [HAVING] [ORDER]";
+
+/*
+ * Questa istruzione imposta un campo extra che avrà, di default, valore nullo.
+ * Di base è impostata a true
+ */
+$oField->multi_select_one = true;
+
+/*
+ * Questa istruzione imposta la scritta che vogliamo sul campo extra.
+ * Di base è "Selezionare Un Elemento.."
+ */
+$oField->multi_select_one_label = "Selezionare Un Elemento..";
+
+/*
+ * Questa istruzione istruzione imposta un valore per il campo extra
+ * N.B. Il valore deve essere di tipo ffData!
+ * Il valore di defaul è null
+ */
+$oField->multi_select_one_val = new ffData(0);
+
+$oField->base_type = "Text";
+$oField->label = "Seleziona utente:";
+$oRecord->addContent($oField, $group_field);
+
+$code = '<div class="' . cm_getClassByFrameworkCss(array(6), "col") . '">
+	<code>
+		$oField = ffField::factory($cm->oPage); <br />
+        $oField->id="utenti"; <br />
+        <br />
+        /*Questa istruzione dirà che questo è un campo più complesso, che nel nostro caso si chiamerà "Selection"*/ <br />
+        $oField->extended_type = "Selection"; <br /> 
+        <br />
+        /* <br />
+         * Nella source_SQL va inserita una SELECT che selezionerà solamente due campi <br />
+         * Il primo sarà quello salvato nel database <br />
+         * Il secondo sarà quello visualizzato dall\'utente <br />
+         */ <br />
+        $oField->source_SQL = "  SELECT ID, username FROM cm_mod_security_users <br />
+                                  WHERE 1 <br />
+                                  [AND] [WHERE] [HAVING] [ORDER]"; <br />
+        <br />
+        /* <br />
+         * Questa istruzione imposta un campo extra che avrà, di default, valore nullo. <br />
+         * Di base è impostata a true <br />
+         */ <br />
+        $oField->multi_select_one = true; <br />
+        <br />
+        /* <br />
+         * Questa istruzione imposta la scritta che vogliamo sul campo extra. <br />
+         * Di base è "Selezionare Un Elemento.." <br />
+         */ <br />
+        $oField->multi_select_one_label = "Selezionare Un Elemento.."; <br />
+        
+        /* <br />
+         * Questa istruzione istruzione imposta un valore per il campo extra <br />
+         * N.B. Il valore deve essere di tipo ffData! <br />
+         * Il valore di defaul è null <br />
+         */ <br />
+        $oField->multi_select_one_val = new ffData(0); <br />
+        <br />
+        $oField->base_type = "Text"; <br />
+        $oField->label = "Seleziona utente:"; <br />
+        $oRecord->addContent($oField, $group_field); <br />
+	</code>
+</div>';
+$oRecord->addContent($code, $group_field);
 
 /**
  * Campi con widget
