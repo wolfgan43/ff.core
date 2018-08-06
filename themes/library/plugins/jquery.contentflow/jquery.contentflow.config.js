ad688498b8126b5f41fc56b1d59d50dfb8afc7d5
wolfgan <wolfgan@gmail.com>
2018-08-06 14:41:27 +0200

    Removed unused themes/library/plugins

--- themes/library/plugins/jquery.contentflow/jquery.contentflow.config.js
+++
@@ -1,5 +0,0 @@
-var contentFlowScriptElement = document.createElement("script");
-
-contentFlowScriptElement.setAttribute('src', ff.base_path + "/themes/library/plugins/jquery.contentflow/contentflow.js");
-
-
--- themes/library/plugins/jquery.mediagallery/jquery.mediagallery.observe.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function(){
-	ff.load(["jquery.ui", "swfobject", "jquery.plugins.mousewheel", "jquery.plugins.mediagallery"], function() {
-		jQuery(".mediagallery").mediaGallery({autoplay:0}); 
-	});
-});
