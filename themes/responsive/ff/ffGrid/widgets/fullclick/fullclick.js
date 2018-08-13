abbbb47a958109b70884e9e78e962be6575bb4f2
wolfgan <wolfgan@gmail.com>
2018-08-13 22:35:21 +0200

    Fix to combo element fix Onclick addClass before call ajax

--- themes/responsive/ff/ffGrid/widgets/fullclick/fullclick.js
+++ themes/responsive/ff/ffGrid/widgets/fullclick/fullclick.js
@@ -12,20 +12,23 @@
                             || jQuery(target).is("a")
                             || jQuery(target).closest("a").length
                             || jQuery(target).is("input")
+                            || jQuery(target).is("select")
                             || jQuery(target).is("textarea")
                             || jQuery(target).is("[onclick]")
                             || jQuery(target).parent().is("[onclick]")
                         )
 							return;
 
-						if(jQuery(this).hasClass("custom"))
-							eval(jQuery(this).closest("TR.clickable").data("url"));
-						else if(jQuery(this).closest("TR.clickable").hasClass("ajax"))
-							ff.ffGrid.dialogOpen(jQuery(this).closest("DIV.ffGrid").data("record"), jQuery(this).closest("TR.clickable").data("url"), undefined, jQuery(this).closest("TR.clickable"));
-						else if(jQuery(this).closest("TR.clickable").data("target"))
-						    window.open(jQuery(this).closest("TR.clickable").data("url"));
-						else
-							window.location.href = jQuery(this).closest("TR.clickable").data("url");
+						if(jQuery(this).hasClass("custom")) {
+                            eval(jQuery(this).closest("TR.clickable").data("url"));
+                        } else if(jQuery(this).closest("TR.clickable").hasClass("ajax")) {
+                            jQuery(this).closest("TR.clickable").addClass("ff-modal-highlight");
+                            ff.ffGrid.dialogOpen(jQuery(this).closest("DIV.ffGrid").data("record"), jQuery(this).closest("TR.clickable").data("url"), undefined, jQuery(this).closest("TR.clickable"));
+                        } else if(jQuery(this).closest("TR.clickable").data("target")) {
+                            window.open(ff.ffPage.goToWithRetUrl(jQuery(this).closest("TR.clickable").data("url"), true));
+                        } else {
+                            ff.ffPage.goToWithRetUrl(jQuery(this).closest("TR.clickable").data("url"));
+                        }
 					}).css("cursor", "pointer");
 					
 					jQuery("TR .ffField.clickable", $table).bind("click.ff.ffGrid.fullclick", function(e) {
@@ -34,20 +37,23 @@
                             || jQuery(target).is("a")
                             || jQuery(target).closest("a").length
                             || jQuery(target).is("input")
+                            || jQuery(target).is("select")
                             || jQuery(target).is("textarea")
                             || jQuery(target).is("[onclick]")
                             || jQuery(target).parent().is("[onclick]")
                         )
                             return;
 
-						if(jQuery(this).hasClass("custom"))
-							eval(jQuery(this).data("url"));
-						else if(jQuery(this).hasClass("ajax"))
-							ff.ffGrid.dialogOpen(jQuery(this).closest("DIV.ffGrid").data("record"), jQuery(this).data("url"), undefined, jQuery(this));
-						else if(jQuery(this).data("target"))
-						    window.open(jQuery(this).data("url"));
-						else
-							window.location.href = jQuery(this).data("url");
+						if(jQuery(this).hasClass("custom")) {
+                            eval(jQuery(this).data("url"));
+                        } else if(jQuery(this).hasClass("ajax")) {
+                            jQuery(this).closest("TR.clickable").addClass("ff-modal-highlight");
+                            ff.ffGrid.dialogOpen(jQuery(this).closest("DIV.ffGrid").data("record"), jQuery(this).data("url"), undefined, jQuery(this));
+                        } else if(jQuery(this).data("target")) {
+                            window.open(ff.ffPage.goToWithRetUrl(jQuery(this).data("url"), true));
+                        } else {
+                            ff.ffPage.goToWithRetUrl(jQuery(this).closest("TR.clickable").data("url"));
+                        }
 					}).css("cursor", "pointer");					
 				});
 			}
