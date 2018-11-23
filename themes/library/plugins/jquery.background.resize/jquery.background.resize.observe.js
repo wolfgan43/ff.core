33b80051d999b7aff6214e5dfb915f319fa0da11
wolfgan <wolfgan@gmail.com>
2018-11-23 14:23:30 +0100

    Remove unused library

--- themes/library/plugins/jquery.background.resize/jquery.background.resize.observe.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function() { 
-	ff.load("jquery.plugins.background-resize", function(){
-		jQuery("body").ezBgResize({
-			img : "/themes/site/images/background.jpg",
-			opacity : 1, 
-			center : true
-		});
-	});
-});
--- themes/library/plugins/jquery.jjmenu/ajax.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-echo '{"state":"ok","returnValue":" server time - ' . date("Y-m-d H:i:s") . ' - myid: '.$_REQUEST['myid'].'"}';
-die;
-
-?>
