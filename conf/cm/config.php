327e3058bc108119c14f2a0470d244d277e75850
Mirko <supporto@xodusweb.com>
2018-12-19 17:55:19 +0100

    Introduzione controlli su ambienti di sviluppo e produzione. introduzione controlli APPID e session_name.

---
+++ conf/cm/config.php
@@ -0,0 +1,24 @@
+<?php
+define("CM_LOCAL_APP_NAME", "not_set");
+if(CM_LOCAL_APP_NAME == "not_set"){
+    die("Inserisci CM_LOCAL_APP_NAME in /conf/cm/config.php");
+}
+
+switch (FF_ENV)
+{
+    case FF_ENV_DEVELOPMENT:
+        define("CM_ENABLE_MEM_CACHING", false);
+        define("CM_FILECACHE", false);
+        define("FF_ERROR_HANDLER_HIDE", false);
+        break;
+
+    case FF_ENV_STAGING:
+        define("CM_ENABLE_MEM_CACHING", false);
+        define("CM_FILECACHE", false);
+        define("FF_ERROR_HANDLER_HIDE", false);
+        break;
+
+    default:
+        define("CM_SHOWFILES_ENABLE_DEBUG", false);
+        define("FF_ERROR_HANDLER_HIDE", true);
+}
